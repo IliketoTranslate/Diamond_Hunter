517a659a8b4f9cc2e8792d614a8abb69d952f97d
Pax81 <padamczyk@poczta.fm>
2023-03-04 14:05:03 +0100

    1. Implemented falling stones from stones 2. Added sequential walking

--- game.py
+++ game.py
@@ -39,6 +39,7 @@ class Game():
             delta_x = 0
             delta_y = 0
             keys = pg.key.get_pressed()
+            pg.time.set_timer(pg.KEYDOWN, 200)
             if keys[pg.K_ESCAPE]:
                 self._done = True
                 self._return_val = GameStatus.ESCAPE_HIT
@@ -60,6 +61,7 @@ class Game():
                 #print("Move player by x="+str(delta_x)+" y="+str(delta_y))
                 self._player.getRect().move_ip(delta_x, delta_y)
         elif event.type == pg.KEYUP:
+            pg.time.set_timer(pg.KEYDOWN, 0)
             self._player.setWalking(False)
         else:
             self._screen.processEvent(event)
@@ -101,15 +103,17 @@ class Game():
 
 
     def moveObject(self, object, delta_x, delta_y):
-        #print("moving object x="+str(delta_x)+" y="+str(delta_y))
         object.getRect().move_ip(delta_x, delta_y)
 
     def moveObjects(self):
+        obj_below = None
         for object in self._objects:
             if object.dropable():
+                #1. check if there is something to fall
                 check_rect = object.getRect().move(0, self._shift)
                 for el in self._objects:
                     if check_rect.contains(el.getRect()):
+                        obj_below = el
                         if object.inFall(): 
                             if el.playable() == True:#unfortunately we found player
                                 self.killPlayer()
@@ -121,6 +125,26 @@ class Game():
                 else:
                     self.moveObject(object, 0, self._shift)
                     object.setInFall(True)
+                    continue
+                if not obj_below == None and obj_below.dropable():
+                    #2. check if there is something on right/left bottom
+                    check_nextto = object.getRect().move(self._shift, 0)
+                    check_below = object.getRect().move(self._shift, self._shift)
+                    for el in self._objects:
+                        if check_nextto.contains(el.getRect()) or check_below.contains(el.getRect()):
+                            break
+                    else:
+                        self.moveObject(object, self._shift, self._shift)
+                        object.setInFall(True)
+                    check_nextto = object.getRect().move(-self._shift, 0)
+                    check_below = object.getRect().move(-self._shift, self._shift)
+                    for el in self._objects:
+                        if check_nextto.contains(el.getRect()) or check_below.contains(el.getRect()):
+                            break
+                    else:
+                        self.moveObject(object, -self._shift, self._shift)
+                        object.setInFall(True)
+
 
     def killPlayer(self):
         if self._chances == 0:
@@ -134,7 +158,7 @@ class Game():
         clock = pg.time.Clock()
         ticks = 0
         while not self._done:
-            ticks += clock.tick(60)
+            ticks += clock.tick()
             self._screen.refresh()
             self._screen.blit(self._state)
             for el in self._objects:
